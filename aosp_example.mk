a567dcc1ab0c9cb83630fab04d2523199e0d3bea
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-01-12 17:08:37 -0300

    Import aosp_example.mk from dipper

---
+++ aosp_example.mk
@@ -0,0 +1,20 @@
+# Inherit some common PixelExperience stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_dipper
+PRODUCT_DEVICE := dipper
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 8
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
+    PRODUCT_NAME="dipper" \
+    TARGET_DEVICE="dipper"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
